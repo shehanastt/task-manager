c52940f56db816f3a8a408218d767ad8cd7c0d8e
Shehgit <shehashehanas@gmail.com>
2025-12-15 12:45:48 +0530

    final commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:`${process.env.BACKEND_URL}/api`,
+    baseURL:'https://task-manager-r93o.onrender.com/api',
     timeout:5000,
 });
 
